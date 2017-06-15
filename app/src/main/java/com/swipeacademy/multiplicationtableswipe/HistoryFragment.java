68b4832dd4a9fab46275b98d99f0b979b6009731
Tony Nguyen <tonynguyen0523@gmail.com>
2017-06-15 17:29:58 -0400

    linechart implemented

--- app/src/main/java/com/swipeacademy/multiplicationtableswipe/HistoryFragment.java
+++ app/src/main/java/com/swipeacademy/multiplicationtableswipe/HistoryFragment.java
@@ -2,6 +2,7 @@ package com.swipeacademy.multiplicationtableswipe;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -16,7 +17,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.formatter.IFillFormatter;
+import com.github.mikephil.charting.interfaces.dataprovider.LineDataProvider;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.utils.EntryXComparator;
 import com.swipeacademy.multiplicationtableswipe.data.TableContract;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -30,6 +45,7 @@ public class HistoryFragment extends Fragment implements LoaderManager.LoaderCal
 
     @BindView(R.id.delete_history_button)Button mDeleteButton;
     @BindView(R.id.history_list_recycler_view)RecyclerView mRecyclerView;
+    @BindView(R.id.history_lineChart)LineChart mLineChart;
 
     private String mTable;
     private static final int HISTORY_LOADER = 0;
@@ -117,12 +133,85 @@ public class HistoryFragment extends Fragment implements LoaderManager.LoaderCal
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
 
         adapter.setCursor(data);
+        setLineChartData(data);
     }
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
         adapter.setCursor(null);
     }
+
+    private void setLineChartData(Cursor data){
+
+        ArrayList<Entry> yVals = new ArrayList<Entry>();
+        final ArrayList<String> xVals = new ArrayList<>();
+        Integer[] userCorrectHistory = getUserCorrectHistory(data);
+
+        for(int i = 0; i < userCorrectHistory.length; i++){
+            yVals.add(new Entry(i,userCorrectHistory[i]));
+            xVals.add(Integer.toString(i + 1));
+        }
+
+        LineDataSet set1;
+
+        if(mLineChart.getData()!= null &&
+                mLineChart.getData().getDataSetCount() > 0){
+            set1 = (LineDataSet)mLineChart.getData().getDataSetByIndex(0);
+            set1.setValues(yVals);
+            mLineChart.getData().notifyDataChanged();
+            mLineChart.notifyDataSetChanged();
+        } else {
+            // create a dataset and give it a type
+            set1 = new LineDataSet(yVals, "DataSet 1");
+
+            set1.setMode(LineDataSet.Mode.LINEAR);
+//            set1.setCubicIntensity(0.0f);
+            //set1.setDrawFilled(true);
+            set1.setDrawCircles(false);
+            set1.setLineWidth(1.8f);
+            set1.setCircleRadius(4f);
+            set1.setCircleColor(Color.WHITE);
+            set1.setHighLightColor(Color.rgb(244, 117, 117));
+            set1.setColor(Color.WHITE);
+            set1.setFillColor(Color.WHITE);
+            set1.setFillAlpha(100);
+            set1.setDrawHorizontalHighlightIndicator(false);
+            set1.setFillFormatter(new IFillFormatter() {
+                @Override
+                public float getFillLinePosition(ILineDataSet dataSet, LineDataProvider dataProvider) {
+                    return -10;
+                }
+            });
+
+            final XAxis xAxis = mLineChart.getXAxis();
+            xAxis.setGranularityEnabled(true);
+            xAxis.setGranularity(0.1f);
+            xAxis.setValueFormatter(new IAxisValueFormatter() {
+                @Override
+                public String getFormattedValue(float value, AxisBase axis) {
+                    return xVals.get((int)value);
+                }
+            });
+
+            LineData lineData = new LineData(set1);
+            lineData.setValueTextSize(9f);
+            lineData.setDrawValues(false);
+
+            mLineChart.setData(lineData);
+        }
+    }
+
+    private Integer[] getUserCorrectHistory(Cursor data){
+
+        ArrayList<Integer> correctHistory = new ArrayList<>();
+        data.moveToFirst();
+        while(!data.isAfterLast()){
+            correctHistory.add(data.getInt(HistoryFragment.COL_ROW_TOTAL_RIGHT));
+            data.moveToNext();
+        }
+        return correctHistory.toArray(new Integer[correctHistory.size()]);
+    }
+
 }
 
 
