5e386f6149ed7193a49b4c9c96bf007230fee2c0
tonynguyen <tonynguyen0523@gmail.com>
2017-07-01 20:51:49 -0400

    home under construction

--- app/src/main/java/com/swipeacademy/multiplicationtableswipe/HomeActivity.java
+++ app/src/main/java/com/swipeacademy/multiplicationtableswipe/HomeActivity.java
@@ -1,12 +1,12 @@
 package com.swipeacademy.multiplicationtableswipe;
 
-import android.support.v4.app.DialogFragment;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
@@ -17,9 +17,12 @@ import butterknife.ButterKnife;
 
 public class HomeActivity extends AppCompatActivity {
 
-    @BindView(R.id.practice_button)Button mPracticeButton;
+//    @BindView(R.id.practice_button)Button mPracticeButton;
     @BindView(R.id.home_adView)AdView mAdView;
     @BindView(R.id.home_toolbar)Toolbar mToolbar;
+    @BindView(R.id.included_home_play_card)CardView mIncludedPlayCard;
+    @BindView(R.id.home_practice_card_view)CardView mPracticeCard;
+    @BindView(R.id.home_history_card_view)CardView mHistoryCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +30,30 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         ButterKnife.bind(this);
         setSupportActionBar(mToolbar);
+
+        mIncludedPlayCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment dialogFragment =  new QuestionAmountDialog();
+                dialogFragment.show(getSupportFragmentManager(), "dialog");
+            }
+        });
+
+        mPracticeCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), PracticeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mHistoryCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), HistoryActivity.class);
+                startActivity(intent);
+            }
+        });
 
         CorrectionsUtil.clearCorrections(this);
         Utility.setCurrentScore(this, 0);
@@ -36,19 +63,26 @@ public class HomeActivity extends AppCompatActivity {
         mAdView.loadAd(adRequest);
     }
 
-    public void letsPlay(View view){
-
-        DialogFragment dialogFragment =  new QuestionAmountDialog();
-        dialogFragment.show(getSupportFragmentManager(), "dialog");
+    static class IncludedLayout{
+        @BindView(R.id.home_play_card_view)CardView playCardView;
+        @BindView(R.id.home_practice_card_view)CardView practiceCardView;
+        @BindView(R.id.home_history_card_view)CardView historyCardView;
     }
 
-    public void myHistory(View view){
-        Intent intent = new Intent(this, HistoryActivity.class);
-        startActivity(intent);
-    }
 
-    public void practice(View view){
-        Intent intent = new Intent(this, PracticeActivity.class);
-        startActivity(intent);
-    }
+//    public void letsPlay(View view){
+//
+//        DialogFragment dialogFragment =  new QuestionAmountDialog();
+//        dialogFragment.show(getSupportFragmentManager(), "dialog");
+//    }
+//
+//    public void myHistory(View view){
+//        Intent intent = new Intent(this, HistoryActivity.class);
+//        startActivity(intent);
+//    }
+//
+//    public void practice(View view){
+//        Intent intent = new Intent(this, PracticeActivity.class);
+//        startActivity(intent);
+//    }
 }
